aa8ac976cb34cc2963892cd6d15322b6cb41c647
William Weckl <william.weckl@gmail.com>
2019-05-14 10:37:26 -0300

    Account management domain using RSpec for testing.

---
+++ domains/account_management/spec/account_management_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe AccountManagement do
+  it do
+    expect(AccountManagement::VERSION).to eq "0.1.0"
+  end
+end
--- domains/account_management/test/account_management_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AccountManagement::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, AccountManagement
-  end
-end
